208daff39765ad442704bbe08cc6953a51cca5dd
cheskams <79020128+cheskams@users.noreply.github.com>
2021-02-13 17:53:31 -0700

    Update Log.java

--- Log.java
+++ Log.java
@@ -1,13 +1,24 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class Log {
 
-	JPanel log;
-	int x,y;
+public class Log extends Component {
 
-	public void move (int stepSize) {
+	JPanel log = new JPanel();
+	int x, y;
+	int logWidth = 20, logHeight = 10;
+	
+	Log (int x, int y) {
+		this.x = x;
+		this.y = y;
+		log.setBounds(x,y,logWidth,logHeight);
+		log.setBackground(Color.black);
 		
 	}
 
-}
+	public void move (int stepSize) {
+		x += stepSize;
+		log.setBounds(x, y, logWidth, logHeight);
+	}
+
+	
