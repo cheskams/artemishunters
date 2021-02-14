ca50ad735af411f3e73c3f909d5bbb031f312f42
RachelRalph <60715884+RachelRalph@users.noreply.github.com>
2021-02-13 22:10:29 -0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+class Test {
+    public static void main(String[] args) {
+        /*try {
+            //Background.BackGui();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }*/
+        Beaver beaver = new Beaver(100,100);
+        beaver.moveBeaver();
+    }
+}
