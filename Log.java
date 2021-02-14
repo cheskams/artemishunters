5a6bc86fe019b439383a2279a021489752274be8
cheskams <79020128+cheskams@users.noreply.github.com>
2021-02-13 21:06:05 -0700

    Update Log.java
    Added log image into constructor

--- Log.java
+++ Log.java
@@ -1,7 +1,12 @@
 import java.awt.*;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.imageio.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.awt.image.ImageObserver;
 import java.text.AttributedCharacterIterator;
-
 import javax.swing.*;
 
 
@@ -15,7 +20,15 @@ public class Log {
 		this.x = x;
 		this.y = y;
 		log.setBounds(x,y,logWidth,logHeight);
-		log.setBackground(Color.black);
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File("src/log.png"));
+		} catch (IOException e) {
+		}
+		JLabel skin = new JLabel(new ImageIcon(img));
+		skin.setIcon(new ImageIcon(new ImageIcon(img).getImage().getScaledInstance(logWidth, logHeight, Image.SCALE_DEFAULT)));
+		skin.setBounds(0,0,logWidth, logHeight);
+		log.add(skin);
 		
 	}
 
